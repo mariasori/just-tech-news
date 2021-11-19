1ea8206d6539db5b2a15235c4d2153b372f4d115
Maria Sori <mariasori.school@gmail.com>
2021-11-18 20:44:08 -0500

    hashing password and allowing login

--- models/User.js
+++ models/User.js
@@ -6,7 +6,12 @@ const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
 
 // create our User model
-class User extends Model {}
+class User extends Model {
+  // set up method to run on instance data (per user) to check password
+  checkPassword(loginPw) {
+    return bcrypt.compareSync(loginPw, this.password);
+  }
+}
 
 // define table columns and configuration
 User.init(
@@ -71,5 +76,7 @@ User.init(
       modelName: 'user'
     }
   );
+
+
 
 module.exports = User;
