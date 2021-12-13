7308a954a171e92456db46a527a7fd470efbcf36
Maria Sori <mariasori.school@gmail.com>
2021-12-12 20:43:32 -0500

    working homepage with seeded data, handlebars and each helper

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const path = require('path');
 const express = require('express');
 const exphbs = require('express-handlebars');
+const routes = require('./controllers/');
+
 
 const app = express();
 const PORT = process.env.PORT || 3001;
